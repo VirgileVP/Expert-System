01bbe4b5f5d55f8f32cb7345130261b558334ac5
VirgileVP <virgilepornet@gmail.com>
2019-12-06 16:09:45 +0100

    upgrading parsing bases functions, adding is_operator and is_fact

--- srcs/main.py
+++ srcs/main.py
@@ -1,18 +1,24 @@
-from parse import Parser
+import parse
 import math
 import os
 import sys
 
 USAGE = 'Usage : python3 main.py [file_location]'
 
-def error(reason):
-	exit(reason + USAGE)
+# error:
+# exit the programm with and error message
+def error(reason, usage):
+	if usage == 1:
+		exit(reason + USAGE)
+	exit(reason)
 
+# get_file:
+# check arguments and return the file
 def get_file():
 	if len(sys.argv) == 1:
-		 error('No arguments\n')
+		error('No arguments\n', 1)
 	elif len(sys.argv) > 2:
-		error('Too many arguments!\n')
+		error('Too many arguments\n', 1)
 	os.getcwd()
 	filename = sys.argv[1]
 	file = open(filename, "r")
@@ -20,7 +26,7 @@ def get_file():
 
 def main():
 	file = get_file()
-	Parser.parsing(file)
+	parse.parsing(file)
 	file.close()
 
 if __name__ == '__main__':
--- srcs/parse.py
+++ srcs/parse.py
@@ -1,23 +1,79 @@
-class Parser:
-	def is_space(char):
-		if char == ' ' or char == '\n' or char == '\t':
-			return 1
-		return 0
+import main
 
-	def is_rules(line):
-		i = 0
-		while self.is_space(line[i]):
-			i += 1
-		if line[i] == '#':
-			return 0
+# is_space:
+# check if the char is a space, a tab or a line-return
+def is_space(char):
+	if char == ' ' or char == '\n' or char == '\t':
 		return 1
+	return 0
 
-	def parsing(file):
-		# ligne par ligne f = file.readline().rstrip('\n\r')
-		rules = {} #dictionaire
-		i = 0
-		for line in file:
-			if self.is_rules() == 1:
-				rules[i] = line
-			# print(rules[i])
-			i += 1
+
+# check_parentheses:
+# check and analyze stuff in parenthese
+def check_parentheses(line, index):
+	# TO DEVELOP
+	return 1
+
+# is_operator:
+# check if it is a valid operator (+, |, =>, <=>, ^)
+def is_operator(line, index):
+	if line[index] == '(':
+		check_parentheses(line, index) # not yet dev
+	if line[index] == '+' or line[index] == '|' or line[index] == '^':
+		return 1
+	if line[index] == '=' and line[index + 1] == '>':
+		return 1
+	if line[index] == '<' and line[index + 1] == '=' and line[index + 2] == '>':
+		return 1
+	return 0
+
+# is_fact:
+# check if the char is a valid fact (only one letter, with or whitout '!' before)
+def is_fact(line, index):
+	if line[index] == '!':
+		if line[index + 1].isalpha() == 1:
+			return 1
+		main.error("Facts must be letter\n", 0)
+		return 0
+	if line[index].isalpha() == 1:
+		return 1
+	main.error("Facts must be letter\n", 0)
+	return 0
+
+# is_rules:
+# check if the line is a valid rules
+def is_rules(line):
+	i = 0
+	while is_space(line[i]):
+		i += 1
+	if line[i] == '#':
+		return 0
+	if is_fact(line, i) == 0:
+		return 0
+	#if is_operator(line, i) == 1:
+
+	else :
+		error("Invalid operator or no operator\n", 0)
+	return 1
+
+# parsing:
+# read and check all lines, store it if they are valid
+def parsing(file):
+	line = file.readline().rstrip('\n\r')
+	rules = {} #dictionaire
+	while line.isspace() or is_comment:
+		line = file.readline().rstrip('\n\r')
+	line = file.readline().rstrip('\n\r')
+	if is_rules(line) != 1:
+		main.error("Rules first", 0)
+	line = file.readline().rstrip('\n\r')
+	while is_rules(line):
+		line = line.split('#')
+		rules[i] = line[0]
+		i += 1
+		line = file.readline().rstrip('\n\r')
+	i = 0
+	for line in file:
+		if is_rules(line) == 1:
+			rules[i] = line
+		i += 1
